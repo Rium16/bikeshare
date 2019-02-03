df188e5b25c10da132b2cf632d9df2946bb164b5
Chay Rice <37002319+Chaymer@users.noreply.github.com>
2019-02-03 20:01:26 +0000

    Add files via upload
    Added the .js for the signin page

---
+++ client/src/views/signin.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+
+export default class Example extends React.Component {
+  render() {
+    return (
+    <div className="signin">
+      <h1 className="signinhead">SIGN IN</h1>
+      <Form>
+        <FormGroup>
+          <Label className = "padded" for="exampleEmail">Email: </Label>
+          <Input className = "padded" type="email" name="email" id="exampleEmail" placeholder="example@host.com" />
+        </FormGroup>
+        <FormGroup>
+          <Label className = "padded" for="examplePassword">Password: </Label>
+          <Input className = "padded" type="password" name="password" id="examplePassword" />
+        </FormGroup>
+        <Button>Sign In</Button>
+      </Form>
+    </div>
+    );
+  }
+}
+
