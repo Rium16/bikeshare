0b7eddc52e04014058ba53615a2d43bf60f3bb18
Jamie <jam10@hw.ac.uk>
2019-03-16 12:48:14 +0000

    darkmode for map and navbars - cba styling for the rest of the pages

---
+++ client/src/services/theme.js
@@ -0,0 +1,18 @@
+
+export function theme(theme) {
+    switch(theme) {
+        case 'dark':
+            return {
+                backgroundColor: '#1b1e21',
+                mapUrl: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+                mapAttribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+            }
+        case 'light':
+        default:
+            return {
+                backgroundColor: '#f8f9fa',
+                mapUrl: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+                mapAttribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+            }
+    }
+}
--- client/src/views/settings_components/SettingsNav.js
+++ client/src/views/settings_components/SettingsNav.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import { Nav, Navbar, NavItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { IoIosArrowRoundBack } from 'react-icons/io';
+import { theme } from '../../services/theme';
 
 class SettingsNav extends Component {
 
     render() {
         return (
-            <Navbar style={{whiteSpace: 'nowrap'}} className="navbar-expand-lg setting-nav" color="light" light expand="md">
+            <Navbar style={{whiteSpace: 'nowrap'}} className="navbar-expand-lg setting-nav" style={{backgroundColor: theme(localStorage.getItem('theme')).backgroundColor}} light expand="md">
 
                 <Nav style={{whiteSpace: 'nowrap'}} className="mr-auto" navbar>
                     <NavItem className="icon"><Link to="/settings"><IoIosArrowRoundBack size={32}/></Link></NavItem>
